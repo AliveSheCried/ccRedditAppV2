244de6bc3fc81f29b7d08fd9534b9de05b230e06
AliveSheCried <robert@hood.gb.net>
2022-01-31 21:40:38 +0000

    Neflify deployment fail fix #2 - step 3

--- src/pages/navigation/Navigation.js
+++ src/pages/navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import { Logo } from "../../components/logo/Logo";
+import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { QuickNav } from "../../components/quicknav/QuickNav";
 import { Search } from "../../components/search/Search";
@@ -7,7 +7,7 @@ import { Search } from "../../components/search/Search";
 export const Navigation = () => {
   return (
     <header>
-      {/* <Logo /> */}
+      <Logo />
       <Search />
       <QuickNav />
       <Menu />
