2e80e8c599ef1436ed69d05b8e59b9dc04039c8c
AliveSheCried <robert@hood.gb.net>
2022-01-31 21:04:59 +0000

    Netflify deployment fail fix - step 2 recreate and repush file

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <div className="logo">
+      <h1>
+        <span className="logo__text--white">r/</span>reddit
+      </h1>
+    </div>
+  );
+};
